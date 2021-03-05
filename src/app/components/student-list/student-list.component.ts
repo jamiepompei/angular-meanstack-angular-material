37b652e4cdc8054d601fa6fed503e5cfe4cfebc7
Nicole Pompei <nicole.lynn.pompei@gmail.com>
2021-03-05 12:29:19 -0500

    added angular material data tables and pagination, render students list in the constructor, added method to delete student from the list

--- src/app/components/student-list/student-list.component.ts
+++ src/app/components/student-list/student-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Student } from './../../shared/student';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { ApiService } from 'src/app/shared/api.service';
+
 
 @Component({
   selector: 'app-student-list',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-list.component.css']
 })
 export class StudentListComponent implements OnInit {
+  StudentData: any = [];
+  dataSource!: MatTableDataSource<Student>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  displayedColumns: string[] = ['_id', 'student_name', 'student_email', 'section', 'action'];
 
-  constructor() { }
+  constructor(private api: ApiService) {
+    this.api.getAllStudents().subscribe(data => {
+      this.StudentData = data;
+      this.dataSource = new MatTableDataSource<Student>(this.StudentData);
+      setTimeout(() => {
+        this.dataSource.paginator = this.paginator;
+      }, 0);
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  deleteStudent(index: number, e: any){
+    if(window.confirm('Are you sure')){
+      const data = this.dataSource.data;
+      data.splice((this.paginator.pageIndex * this.paginator.pageSize) + index, 1);
+      this.dataSource.data = data;
+      this.api.deleteStudent(e._id).subscribe()
+    }
   }
 
 }
